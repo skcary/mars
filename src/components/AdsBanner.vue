919c51ec67fb7b2f6a95891f143f727f9c3a0c34
沈凯 <kshen@alibaba-inc.com>
2017-12-04 18:37:48 +0800

    update

---
+++ src/components/AdsBanner.vue
@@ -0,0 +1,46 @@
+<template>
+  <slider
+    class="slider"
+    :interval="3000"
+    :auto-play="true">
+    <div
+      v-for="(item, index) in list"
+      :key="index"
+      :href="item.url">
+      <image class="image" resize="cover" :src="item.pic"></image>
+    </div>
+    <indicator v-if="list.length > 1" class="indicator"></indicator>
+  </slider>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style scoped>
+.slider {
+  height: 150px;
+}
+.image {
+  width: 750px;
+  height: 150px;
+}
+.indicator {
+  position: absolute;
+  z-index: 1;
+  bottom: 12px;
+  width: 750px;
+  height: 12px;
+  item-color: #fff;
+  item-selected-color: #ffc900;
+  item-size: 12px;
+  justify-content: center;
+}
+</style>
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export default {
+  b:[
+    {
+      url:"http://www.sina.com.cn/",
+      pic:"https://img.alicdn.com/tfs/TB1.mmrfJfJ8KJjy0FeXXXKEXXa-750-180.jpg"
+    },
+    {
+      url:"http://www.taobao.com",
+      pic:"https://img.alicdn.com/imgextra/i3/2353981359/TB2hXKAd4PI8KJjSspfXXcCFXXa_!!2353981359.jpg"
+    }
+  ]
+}
