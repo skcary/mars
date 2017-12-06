613d3c1c679b86122a6556e86e536e9fb1f36fb4
沈凯 <kshen@alibaba-inc.com>
2017-12-06 18:14:41 +0800

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,59 @@
 <template>
-  
+  <div class="wrapper">
+    <list style="flex:1">
+      <!-- 头部 -->
+      <cell>
+        <home-header></home-header>
+      </cell>
+      <!-- 轮播 -->
+      <cell>
+        <div class="space"></div>
+        <home-banner :list="homeBannerList"></home-banner>
+      </cell>
+      <!-- tag -->
+      <cell>
+        <home-tag :list="homeTagList"></home-tag>
+      </cell>
+      <!-- 广告位 -->
+      <cell v-if="adBannerList && adBannerList.length">
+        <div class="space"></div>
+        <ads-banner :list="adBannerList"></ads-banner>
+      </cell>
+    </list>
+  </div>
 </template>
 
 <script>
+import data from "../data";
+import HomeHeader from "../components/HomeHeader.vue";
+import HomeBanner from "../components/HomeBanner.vue";
+import HomeTag from "../components/HomeTag.vue";
+import AdsBanner from "../components/AdsBanner.vue";
 export default {
-  
-}
+  components: { HomeHeader, HomeBanner, HomeTag, AdsBanner },
+  data() {
+    return {
+      bannerList: null
+    };
+  },
+  created() {
+    this.homeBannerList = data.hb;
+    this.homeTagList = data.ht;
+    this.adBannerList = data.b;
+  },
+  mounted() {}
+};
 </script>
 
 <style scoped>
-
+.wrapper {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.space {
+  height: 24px;
+}
 </style>
